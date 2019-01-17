40218cb8b6ae566b5711dbc85f6cceab77006018
Gabriel Solomon <hello@gabrielsolomon.ro>
2019-01-17 16:24:49 +0200

    add method to generate MessageString on violation bag

--- src/Validation/Violations/ViolationsBag.php
+++ src/Validation/Violations/ViolationsBag.php
@@ -13,4 +13,16 @@ use JsonSerializable;
  */
 class ViolationsBag extends \Nip\Collections\Collection
 {
+    /**
+     * @param string $glue
+     * @return string
+     */
+    public function getMessageString($glue = ', ')
+    {
+        $messages = [];
+        foreach ($this as $violation) {
+            $messages[] = $violation->getMessage();
+        }
+        return implode($glue, $messages);
+    }
 }
