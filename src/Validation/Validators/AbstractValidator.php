a526a61844ea796ce423863f8e304be9af7ac58b
Gabriel Solomon <hello@gabrielsolomon.ro>
2019-01-17 15:46:36 +0200

    fix add Violation

--- src/Validation/Validators/AbstractValidator.php
+++ src/Validation/Validators/AbstractValidator.php
@@ -5,8 +5,10 @@ namespace ByTIC\MediaLibrary\Validation\Validators;
 use ByTIC\MediaLibrary\Collections\Collection;
 use ByTIC\MediaLibrary\Exceptions\UnexpectedTypeException;
 use ByTIC\MediaLibrary\Media\Media;
+use ByTIC\MediaLibrary\Validation\Constraints\AbstractConstraint;
 use ByTIC\MediaLibrary\Validation\Constraints\ConstraintInterface;
 use ByTIC\MediaLibrary\Validation\Traits\HasValidatorTrait;
+use ByTIC\MediaLibrary\Validation\Violations\Violation;
 use ByTIC\MediaLibrary\Validation\Violations\ViolationsBag;
 use Nip\Utility\Traits\NameWorksTrait;
 
@@ -139,11 +141,19 @@ abstract class AbstractValidator implements ValidatorInterface
     }
 
     /**
-     * @param $attribute
+     * @param AbstractConstraint $constraint
+     * @param $code
      * @param $parameters
      */
     protected function addViolation($constraint, $code, $parameters)
     {
+        $violation = new Violation();
+        $violation->setCode($code);
+        $violation->setMessage($constraint::errorNames[$code]);
+        $violation->setParameters($parameters);
 
+        $this->violations->add(
+            $violation
+        );
     }
 }
--- src/Validation/Violations/Violation.php
+++ src/Validation/Violations/Violation.php
@@ -8,5 +8,55 @@ namespace ByTIC\MediaLibrary\Validation\Violations;
  */
 class Violation
 {
+    protected $code;
+    protected $message;
+    protected $parameters;
 
+    /**
+     * @return mixed
+     */
+    public function getCode()
+    {
+        return $this->code;
+    }
+
+    /**
+     * @param mixed $code
+     */
+    public function setCode($code)
+    {
+        $this->code = $code;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMessage()
+    {
+        return $this->message;
+    }
+
+    /**
+     * @param mixed $message
+     */
+    public function setMessage($message)
+    {
+        $this->message = $message;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getParameters()
+    {
+        return $this->parameters;
+    }
+
+    /**
+     * @param mixed $parameters
+     */
+    public function setParameters($parameters)
+    {
+        $this->parameters = $parameters;
+    }
 }
